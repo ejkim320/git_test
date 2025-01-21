986eead4df9ce2efdda19cbb22ec13d8433099df
김은재 <ejkim320@inu.ac.kr>
2025-01-21 13:23:50 +0900

    main - push button code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, \
+    QHBoxLayout, QVBoxLayout, QMessageBox
 
 # 클래스 선언 (클래스 상속)
 class Calculcator(QWidget):
@@ -12,9 +13,21 @@ class Calculcator(QWidget):
         self.initUI()
     
     def initUI(self):
+
+        # ------------1차 생성 추가 부분------------
+        # 윈도우 버튼 생성
+        self.btn1 = QPushButton('Message,', self)
+        # 해당하는 버튼이 클릭됐을 때 이벤트 발생 (activeMessage 함수 실행)
+        self.btn1.clicked.connect(self.activeMessage)
+
         self.setWindowTitle("Calculator") # 새로운 화면에 제목
         self.resize(256, 256) # 새로운 화면의 사이즈
         self.show() # 윈도우 화면을 출력
+    
+    # 버튼 클릭 시 실행할 이벤트 함수 생성
+    def activeMessage(self):
+        # 버튼 클릭 시 메시지 박스 출력 
+        QMessageBox.information(self, 'information', 'Button clicked!')
 
 if __name__ == "__main__": 
     app = QApplication(sys.argv) # 클래스 생성
